d1d4aeed908b3710fe7787e0d227a715d3a35fa2
Rajan Sahu <55847984+rajansahu713@users.noreply.github.com>
2022-01-05 23:14:42 +0530

    Add files via upload

---
+++ Django-rest-api/app/app/fn_api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, default="") 
+
+    def __str__(self):
+        return self.product_name
